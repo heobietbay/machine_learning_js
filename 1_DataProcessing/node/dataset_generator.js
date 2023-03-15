feaf49be08d6a970dd4cb5cd3b70945f2fcdf8c2
Tran Dang Khoa <tetsusoiwashiro@gmail.com>
2023-03-15 22:15:02 +0700

    Data processing

---
+++ 1_DataProcessing/node/dataset_generator.js
@@ -0,0 +1,20 @@
+const constants = {};
+constants.DATA_DIR = "../data";
+constants.RAW_DIR = constants.DATA_DIR + "/raw";
+constants.DATASET_DIR = constants.DATA_DIR + "/dataset";
+constants.JSON_DIR = constants.DATASET_DIR + "/json";
+constants.IMG_DIR = constants.DATASET_DIR + "/img";
+constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
+
+const fs = require('fs');
+const fileNames = fs.readdirSync(constants.RAW_DIR);
+const samples = [];
+
+let id = 1;
+fileNames.forEach(fn => {
+    const fcontent = fs.readFileSync(`${constants.RAW_DIR}/${fn}`)
+    const jsonContent = JSON.parse(fcontent);
+    const { session : student_id, student : student_name, drawings } = jsonContent;
+    console.log(student_name);
+}
+);

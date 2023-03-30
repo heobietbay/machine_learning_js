aeeac2e0c71388eccb8b2979dd7a360bcca4b091
heobietbay <trandangkhoa@tma.com.vn>
2023-03-30 21:41:38 +0700

    Write to samples.js

--- 1_DataProcessing/common/constants.js
+++ 1_DataProcessing/common/constants.js
@@ -7,6 +7,9 @@ constants.JSON_DIR = constants.DATASET_DIR + "/json";
 constants.IMG_DIR = constants.DATASET_DIR + "/img";
 constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
 
+constants.JS_OBJECTS = "../common/js_objects";
+constants.SAMPLES_JS = constants.JS_OBJECTS + "/samples.js";
+
 if(typeof module !== 'undefined') {
   module.exports = constants;
 }
--- 1_DataProcessing/node/dataset_generator.js
+++ 1_DataProcessing/node/dataset_generator.js
@@ -3,7 +3,7 @@ const constants = require('../common/constants.js');
 const utils = require('../common/utils.js');
 
 const { createCanvas } = require('canvas');
-const canvas = createCanvas(400,400);
+const canvas = createCanvas(400, 400);
 const ctx = canvas.getContext('2d');
 
 const fs = require('fs');
@@ -11,34 +11,43 @@ const fileNames = fs.readdirSync(constants.RAW_DIR);
 const samples = [];
 
 let id = 1;
+
+// each file would have 8 samples.
+const totalSamples = fileNames.length * 8;
 fileNames.forEach(fn => {
-    const fcontent = fs.readFileSync(`${constants.RAW_DIR}/${fn}`)
-    const jsonContent = JSON.parse(fcontent);
-    const { session: student_id, student: student_name, drawings } = jsonContent;
+  const fcontent = fs.readFileSync(`${constants.RAW_DIR}/${fn}`)
+  const jsonContent = JSON.parse(fcontent);
+  const { session: student_id, student: student_name, drawings } = jsonContent;
 
-    for (let label in drawings) {
-        samples.push({
-            id,
-            label,
-            student_name,
-            student_id
-        });
-        const paths = drawings[label];
-        fs.writeFileSync(`${constants.JSON_DIR}\/${id}.json`, JSON.stringify(paths));
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        generateImageFromPaths(`${constants.IMG_DIR}\/${id}.png`, paths, ctx);
+  for (let label in drawings) {
+    samples.push({
+      id,
+      label,
+      student_name,
+      student_id
+    });
+    const drawingPaths = drawings[label];
+    renderImageToFile(drawingPaths);
+    utils.printProgress(id, totalSamples);
+    id++;
+  }
 
-        // each file would have 8 samples.
-        utils.printProgress(id, fileNames.length * 8);
-        id++;
-    }
-}
-);
+  function renderImageToFile(paths) {
+    fs.writeFileSync(`${constants.JSON_DIR}\/${id}.json`, JSON.stringify(paths));
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    generateImageFromPaths(`${constants.IMG_DIR}\/${id}.png`, paths, ctx);
+  }
+});
+
 fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
 
-function generateImageFromPaths(outFile, paths, ctx) {
-    draw.drawPaths(ctx, paths);
+// doing this, so 0_DrawingApp can import the samples.
+// Technically, this is to by pass server side development, CORS and other stuff.
+fs.writeFileSync(constants.SAMPLES_JS, `const samples = ${JSON.stringify(samples)};`);
 
-    const buffer = canvas.toBuffer('image/png');
-    fs.writeFileSync(outFile, buffer);
+function generateImageFromPaths(outFile, paths, ctx) {
+  draw.drawPaths(ctx, paths);
+
+  const buffer = canvas.toBuffer('image/png');
+  fs.writeFileSync(outFile, buffer);
 }

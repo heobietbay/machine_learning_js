a31be5f0014198ff168bae015f085942caecc9c0
trandangkhoa <tetsusoiwashiro@gmail.com>
2023-04-05 10:41:17 +0700

    styling

--- 0_DrawingApp/js/display.js
+++ 0_DrawingApp/js/display.js
@@ -3,17 +3,44 @@ function createRow(container, studentName, samples) {
     row.classList.add('row');
     container.appendChild(row);
 
-    const rowLabel = document.createElement('div');
-    rowLabel.innerHTML = studentName;
-    rowLabel.classList.add('rowLabel');
+    const rowLabel = renderRowLabel();
     row.appendChild(rowLabel);
 
-    for(let sample of samples) {
-       const {id, label} = sample;
-       const img = document.createElement('img');
-       img.setAttribute('loading', 'lazy');
-       img.src = constants.IMG_DIR + '/' + id + '.png';
-       img.classList.add('thumb');
-       row.appendChild(img);
+    for (let sample of samples) {
+        const { id, label } = sample;
+
+        const sampleContainer = createSampleContainer(id);
+
+        const sampleLabel = document.createElement('div');
+        sampleLabel.innerHTML = label;
+        sampleContainer.appendChild(sampleLabel);
+
+        const img = renderImg(id);
+        sampleContainer.appendChild(img);
+
+        row.appendChild(sampleContainer);
+    }
+
+
+    function createSampleContainer(id) {
+        const sampleContainer = document.createElement('div');
+        sampleContainer.id = `sample_${id}`;
+        sampleContainer.classList.add('sampleContainer');
+        return sampleContainer;
+    }
+
+    function renderRowLabel() {
+        const rowLabel = document.createElement('div');
+        rowLabel.innerHTML = studentName;
+        rowLabel.classList.add('rowLabel');
+        return rowLabel;
+    }
+
+    function renderImg(id) {
+        const img = document.createElement('img');
+        img.setAttribute('loading', 'lazy');
+        img.src = constants.IMG_DIR + '/' + id + '.png';
+        img.classList.add('thumb');
+        return img;
     }
 }
